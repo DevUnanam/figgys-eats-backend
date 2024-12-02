af19aee82a18511cb819c965fa21e3df48ef1cd8
DevUnanam <riaahh20@yahoo.com>
2024-12-02 07:27:36 -0800

    more

--- figgys_eats/figgys_eats/wsgi.py
+++ figgys_eats/figgys_eats/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'figgys_eats.figgys_eats.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'figgys_eats.settings')
 
 application = get_wsgi_application()
